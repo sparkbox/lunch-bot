f5271aa7c3998555ef31ca053e39f5ebd8dddccf
Adam Simpson <adam@heysparkbox.com>
2016-02-25 10:48:03 -0500

    fix: post messages using `as_user` param.
    See [the postMessage docs][api] for info on the `as_user` param.
    
    [api]: https://api.slack.com/methods/chat.postMessage

--- src/postToSlack.js
+++ src/postToSlack.js
@@ -3,7 +3,10 @@ import Slack from 'slack-client';
 const postToSlack = (channel, msg) => {
   const slack = new Slack.WebClient(process.env.slackToken);
 
-  slack.chat.postMessage(channel, msg, {"unfurl_links": true});
+  slack.chat.postMessage(channel, msg, {
+    "unfurl_links": true,
+    "as_user": true
+  });
 }
 
 export { postToSlack };
