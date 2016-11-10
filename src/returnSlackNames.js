3bded66984dbcd929e7e222f4e7795d06eb7f3f7
Adam Simpson <adam@heysparkbox.com>
2016-11-10 16:19:05 -0500

    :art: format names correctly for general msg

--- src/returnSlackNames.js
+++ src/returnSlackNames.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 
-const returnSlackNames = (names, slackNamesMap) => {
+const returnSlackNames = (slackIds, names, slackNamesMap) => {
   const slackNames = slackNamesMap.map((x, i) => {
     if (_.includes(names, x.content)) {
-      return slackNamesMap[i + 1].content;
+      const name = slackNamesMap[i + 1].content;
+
+      if (slackIds) {
+        const id = slackIds[`${name}`];
+        return `${id}|${name}`;
+      }
+
+      return name;
     }
     return false;
-  }).filter((x) => x);
+  }).filter(x => x);
 
   return slackNames;
 };
